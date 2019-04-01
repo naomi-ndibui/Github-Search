1188ee62c88241173132b4ea3b88f8b819c15e35
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-01 10:30:21 +0300

    added a service

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from './../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  userName: string;
+  apiUrl: string = "https://api.github.com/users/";
+
+
+  constructor(public http: HttpClient) {
+  this.userName = "naomi-ndibui";
+  }
+
+  getUserDetails() {
+
+    return this.http.get(`https://api.github.com/users/${this.userName}?access_token=${environment.apiKey}`)
+    // return this.http.get(this.apiUrl + this.userName + "?access_token=" + environment.apiKey)
+  }
+  getRepos() {
+    return this.http.get(this.apiUrl + this.userName + "/repos?access_token=" + environment.apiKey)
+  }
+  updateUserName(userName: string) {
+    this.userName = userName;
+
+  }
+}
